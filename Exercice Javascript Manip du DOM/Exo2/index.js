5312e1a56809b40cc179aff0a302fb1bc3ef751d
Chaibouu <chaibouabdoulwahab@gmail.com>
2023-08-16 01:03:35 +0100

    add exo2

--- Exercice Javascript Manip du DOM/Exo2/index.js
+++ Exercice Javascript Manip du DOM/Exo2/index.js
@@ -1,4 +1,13 @@
 // 1
+    let red = document.querySelectorAll('.red');
+    let blue = document.querySelector('.blue');
+    let boite = document.querySelector('#paroles');
+    for (let i = 0; i < red.length; i++) {
+        red[i].classList.remove('red');
+        red[i].classList.add('blue');
+        
+        
+    }
 
 // 2
 
