4ef4425717f7b713bad76464908e166a783955df
Chaibouu <chaibouabdoulwahab@gmail.com>
2023-08-20 18:42:28 +0100

    add exo2

--- Exercice Javascript Manip du DOM/Exo4/index.js
+++ Exercice Javascript Manip du DOM/Exo4/index.js
@@ -28,12 +28,148 @@
         const paragraphe = document.createElement('p');
         for (let j = 0; j < tab1.contenu.length; j++) {
             paragraphe.appendChild(document.createTextNode(tab1.contenu[j]));
-            
-            
+            if (j < tab1.contenu.length) {
+                paragraphe.appendChild(document.createElement('br'));
+            }
+            // Une conditions pour ajouter des classes a fin de les afficher ou les masquer
+            if(tab1.type === 'verset'){
+                paragraphe.setAttribute('class','verset')
+            }
+            else if(tab1.type === 'choeur'){
+                paragraphe.setAttribute('class','choeur')
+            }
+            else if(tab1.type === 'pont'){
+                paragraphe.setAttribute('class','pont')
+            }
+            else{
+                paragraphe.setAttribute('class','outro')
+            }
         }
+        div.append(paragraphe)
     }
    
     
 // 3.
+    const footer = document.createElement('footer');
+    let p = document.createElement('p');
+    p.textContent='© Copyright 2023 - Codeloccol';
+    footer.append(p);
+    body.append(footer);
 
 // 4.
+    let verset = document.querySelectorAll('.verset');
+    let choeur = document.querySelectorAll('.choeur');
+    let pont = document.querySelectorAll('.pont');
+    let outro = document.querySelectorAll('.outro');
+    const boitelabel = document.createElement('div');
+    body.prepend(boitelabel);
+    let labelparoles = document.createElement('label');
+    labelparoles.textContent='Masquer les paroles';
+    let labelverset = document.createElement('label');
+    labelverset.textContent='Masquer les versets';
+    let labelchoeur = document.createElement('label');
+    labelchoeur.textContent='Masquer les choeurs';
+    let labelpont = document.createElement('label');
+    labelpont.textContent='Masquer les ponts';
+    let labeloutro = document.createElement('label');
+    labeloutro.textContent='Masquer les outros';
+    let inputparoles = document.createElement('input');
+    inputparoles.id = 'checked';
+    inputparoles.type = 'checkbox';
+    let inputverset = document.createElement('input');
+    inputverset.id = 'checked';
+    inputverset.type = 'checkbox';
+    let inputchoeur = document.createElement('input');
+    inputchoeur.id = 'checked';
+    inputchoeur.type = 'checkbox';
+    let inputpont = document.createElement('input');
+    inputpont.id = 'checked';
+    inputpont.type = 'checkbox';
+    let inputoutro = document.createElement('input');
+    inputoutro.id = 'checked';
+    inputoutro.type = 'checkbox';
+    labelparoles.appendChild(inputparoles);
+    labelverset.appendChild(inputverset);
+    labelchoeur.appendChild(inputchoeur);
+    labelpont.appendChild(inputpont);
+    labeloutro.appendChild(inputoutro);
+    boitelabel.appendChild(labelparoles);
+    boitelabel.appendChild(labelverset);
+    boitelabel.appendChild(labelchoeur);
+    boitelabel.appendChild(labelpont);
+    boitelabel.appendChild(labeloutro);
+    let hr = document.createElement('hr');
+    boitelabel.appendChild(hr);
+    labelparoles.style.marginRight='20px';
+    labelverset.style.marginRight='20px';
+    labelchoeur.style.marginRight='20px';
+    labelpont.style.marginRight='20px';
+
+    inputparoles.addEventListener('input',(e)=>{
+        if(e.target.checked){
+            div.style.display='none'
+            labelparoles.firstChild.data = 'Afficher les paroles';
+        }
+        else{
+            div.style.display='block';
+            labelparoles.firstChild.data = 'Masquer les paroles';
+        }
+    })
+    inputverset.addEventListener('input',(e)=>{
+        // for (let i = 0; i < verset.length; i++) {
+        //     console.log(verset[i]);
+        //     if(e.target.checked){
+        //       verset[i].classList.add('hidden')
+                
+        //     }
+            
+        // }
+        if (e.target.checked) {
+            verset.forEach(element =>{
+                // element.classList.add('hidden')
+                element.style.display = 'none';
+            })
+        }
+        else{
+            verset.forEach(element =>{
+                // element.classList.add('hidden')
+                element.style.display = 'block';
+            })
+        }
+    })
+    inputchoeur.addEventListener('input',(e)=>{
+        if (e.target.checked) {
+            choeur.forEach(element =>{
+                element.style.display = 'none';
+            })
+        }
+        else{
+            choeur.forEach(element =>{
+                element.style.display = 'block';
+            })
+        }
+    })
+    inputpont.addEventListener('input',(e)=>{
+        if (e.target.checked) {
+            pont.forEach(element =>{
+                element.style.display = 'none';
+            })
+        }
+        else{
+            pont.forEach(element =>{
+                element.style.display = 'block';
+            })
+        }
+    })
+    inputoutro.addEventListener('input',(e)=>{
+        if (e.target.checked) {
+            outro.forEach(element =>{
+                element.style.display = 'none';
+            })
+        }
+        else{
+            outro.forEach(element =>{
+                element.style.display = 'block';
+            })
+        }
+    })
