3c1f0aaed77cd1c117e4475ab484c9314b8a64f7
Chaibouu <chaibouabdoulwahab@gmail.com>
2023-08-22 00:22:26 +0100

    add exo5

--- Exercice Javascript Manip du DOM/Exo5/index.js
+++ Exercice Javascript Manip du DOM/Exo5/index.js
@@ -33,7 +33,7 @@
     profil.textContent = datas.profil;
 // 3.
     let description = document.querySelector('#description');
-    description.textContent = datas.description;
+    description.textContent = datas.descripti;
 // 4.
 
 // 5.
