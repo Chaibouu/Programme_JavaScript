2595af6f4e2af8c1c4eaac423d7d588eeca28f97
Chaibouu <chaibouabdoulwahab@gmail.com>
2023-08-21 16:06:32 +0100

    add exo5

---
+++ Exercice Javascript Manip du DOM/Exo5/index.js
@@ -0,0 +1,41 @@
+// -console.log(datas);
+// Structure de datas:
+// - fullname: string
+// - profil: string
+// - email : string
+// - description : string
+// - socials : [Social, Social, ...]
+// - works : [Work, Work, ...]
+// - experience : [Experience, Experience, ...]
+
+// Structure de Social:
+// name : string
+// url : string
+// link : string
+
+// Structure de Work:
+// name: string,
+// url: string,
+// technology: [Techo, Techno, ...],
+// preview: string
+
+// Structure de Experience:
+// name: string,
+// link: string,
+// technology: [Techo, Techno, ...]
+
+// 1.
+
+// 2.
+
+// 3.
+
+// 4.
+
+// 5.
+
+// 6.
+
+// 7.
+
+// 8.
