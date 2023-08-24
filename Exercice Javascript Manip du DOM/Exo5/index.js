8ea5e140a9e655c0ecbe1597d1b7cf5326e572dd
Chaibouu <chaibouabdoulwahab@gmail.com>
2023-08-24 02:29:06 +0100

    add exo5

--- Exercice Javascript Manip du DOM/Exo5/index.js
+++ Exercice Javascript Manip du DOM/Exo5/index.js
@@ -52,8 +52,65 @@
 // 6.
     let works = document.querySelector('#works');
     console.log(works);
-    
+    for (let i = 0; i < datas.works.length; i++) {
+        let div1 = document.createElement('div');
+        div1.setAttribute('class','col-xl-4 col-lg-4 col-sm-6 col-12 my-2');
+        works.append(div1);
+        let div2 = document.createElement('div');
+        div2.setAttribute('class','card');
+        div1.append(div2);
+        let imgg = document.createElement('img');
+        imgg.setAttribute('class','card-img-top');
+        imgg.src = datas.works[i].preview;
+        imgg.alt = datas.works[i].name;
+        div2.prepend(imgg);
+        let div3 = document.createElement('div');
+        div3.setAttribute('class','card-body w-100 px-0');
+        div2.append(div3);
+        let h22 = document.createElement('h2');
+        h22.setAttribute('class','card-title mt-0 mb-4 text-center');
+        h22.textContent = datas.works[i].name;
+        div3.append(h22);
+        let ull = document.createElement('ul');
+        ull.setAttribute('class','list-unstyled w-100 d-flex justify-content-center flex-wrap')
+        let compteur = datas.works[i];
+        div3.append(ull);
+        for (let j = 0; j < compteur.technology.length; j++) {
+            let lii = document.createElement('li');
+            ull.append(lii);
+            let spann = document.createElement('span');
+            spann.setAttribute('class','card-link');
+            spann.textContent = compteur.technology[j];
+            lii.append(spann);
+        }
+        let div4 = document.createElement('div');
+        div4.setAttribute('class','row w-100');
+        div3.append(div4);
+        let div5 = document.createElement('div');
+        div5.setAttribute('class','d-flex justify-content-center');
+        div4.append(div5);
+        let aa = document.createElement('a');
+        aa.setAttribute('class','text-decoration-none px-4 py-3 mt-2 rounded');
+        aa.href = compteur.url;
+        aa.textContent = "See Project";
+        div5.append(aa);
+    }
 
 // 7.
-
+    let experiences = document.querySelector('#experiences')
 // 8.
+{/* <div class="col-xl-4 col-lg-4 col-sm-6 col-12 my-2">
+<div class="card h-100">
+  <img class="card-img-top" src="datasExperiencessUrl" alt="datasExperiencessName">
+  <div class="card-body w-100 px-0">
+    <h2 class="card-title mt-0 mb-4 text-center">datasExperiencessName</h2>
+    <ul class="list-unstyled w-100 d-flex justify-content-center flex-wrap">
+      <!-- 
+        <li>
+          <span class="card-link">datasExperiencesTechnology</span>
+        </li>
+      -->
+    </ul>
+  </div>
+</div>
+</div> */}
